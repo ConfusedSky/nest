56c4c5572e2861fb129208863ce7eb26e624e345
Masa Maeda <masa@mortgagecoach.com>
2022-06-20 00:37:25 -0700

    Adds a new trait for sending values over to the vm

--- src/wren/wren_value.rs
+++ src/wren/wren_value.rs
@@ -78,9 +78,118 @@ impl WrenValue for f64 {
 impl WrenValue for bool {
     const ADDITIONAL_SLOTS_NEEDED: Slot = 0;
     unsafe fn send_to_vm(&self, vm: VMPtr, slot: Slot) {
-        vm.set_slot_bool_unchecked(slot, *self)
+        vm.set_slot_bool_unchecked(slot, *self);
     }
     unsafe fn get_from_vm(vm: VMPtr, slot: Slot) -> Self {
         vm.get_slot_bool_unchecked(slot)
+    }
+}
+
+pub trait WrenArgs {
+    fn get_required_slots(self) -> Slot;
+    fn set_wren_stack(self, vm: VMPtr);
+}
+
+impl<T: WrenValue> WrenArgs for &T {
+    fn get_required_slots(self) -> Slot {
+        1 + T::ADDITIONAL_SLOTS_NEEDED
+    }
+    fn set_wren_stack(self, vm: VMPtr) {
+        vm.ensure_slots(self.get_required_slots());
+        // This is guarenteed to be safe because we ensured that we had enough
+        // slots for T above
+        unsafe {
+            self.send_to_vm(vm, 0);
+        }
+    }
+}
+
+// TODO: Convert this implementation to a macro
+impl<T: WrenValue, U: WrenValue> WrenArgs for (&T, &U) {
+    fn get_required_slots(self) -> Slot {
+        [
+            T::ADDITIONAL_SLOTS_NEEDED + 1,
+            U::ADDITIONAL_SLOTS_NEEDED + 2,
+        ]
+        .into_iter()
+        .max()
+        .unwrap_or(1)
+    }
+
+    fn set_wren_stack(self, vm: VMPtr) {
+        vm.ensure_slots(self.get_required_slots());
+        // This is guarenteed to be safe because we ensured that we had enough
+        // slots for T above
+        unsafe {
+            self.0.send_to_vm(vm, 0);
+            self.1.send_to_vm(vm, 1);
+        }
+    }
+}
+
+impl<T: WrenValue, U: WrenValue, V: WrenValue> WrenArgs for (&T, &U, &V) {
+    fn get_required_slots(self) -> Slot {
+        [
+            T::ADDITIONAL_SLOTS_NEEDED + 1,
+            U::ADDITIONAL_SLOTS_NEEDED + 2,
+            V::ADDITIONAL_SLOTS_NEEDED + 3,
+        ]
+        .into_iter()
+        .max()
+        .unwrap_or(1)
+    }
+
+    fn set_wren_stack(self, vm: VMPtr) {
+        vm.ensure_slots(self.get_required_slots());
+        // This is guarenteed to be safe because we ensured that we had enough
+        // slots for T above
+        unsafe {
+            self.0.send_to_vm(vm, 0);
+            self.1.send_to_vm(vm, 1);
+            self.2.send_to_vm(vm, 2);
+        }
+    }
+}
+
+impl<T: WrenValue, U: WrenValue, V: WrenValue, W: WrenValue> WrenArgs for (&T, &U, &V, &W) {
+    fn get_required_slots(self) -> Slot {
+        [
+            T::ADDITIONAL_SLOTS_NEEDED + 1,
+            U::ADDITIONAL_SLOTS_NEEDED + 2,
+            V::ADDITIONAL_SLOTS_NEEDED + 3,
+            W::ADDITIONAL_SLOTS_NEEDED + 4,
+        ]
+        .into_iter()
+        .max()
+        .unwrap_or(1)
+    }
+
+    fn set_wren_stack(self, vm: VMPtr) {
+        vm.ensure_slots(self.get_required_slots());
+        // This is guarenteed to be safe because we ensured that we had enough
+        // slots for T above
+        unsafe {
+            self.0.send_to_vm(vm, 0);
+            self.1.send_to_vm(vm, 1);
+            self.2.send_to_vm(vm, 2);
+            self.3.send_to_vm(vm, 3);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::WrenArgs;
+
+    // TODO: Figure out how to test set_wren_stack
+
+    #[test]
+    fn test_slot_size() {
+        assert_eq!(1.0.get_required_slots(), 1);
+        assert_eq!((&1.0, &2.0).get_required_slots(), 2);
+        assert_eq!((&vec![vec![1.0]], &2.0).get_required_slots(), 3);
+        assert_eq!((&2.0, &vec![vec![1.0]]).get_required_slots(), 4);
+        assert_eq!((&1.0, &2.0, &3.0).get_required_slots(), 3);
+        assert_eq!((&1.0, &2.0, &3.0, &4.0).get_required_slots(), 4);
     }
 }
