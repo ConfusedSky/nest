ae9717bcb712a2e0662c4e4cd0f4ef6a015e7ebd
Masa Maeda <masa@mortgagecoach.com>
2022-06-28 22:01:18 -0700

    Fixes up tests

--- wren-macros/tests/to_signature.rs
+++ wren-macros/tests/to_signature.rs
@@ -3,10 +3,11 @@ use wren_macros::to_signature;
 const TEST: &[u8] = to_signature!(testThing());
 const TEST2: &[u8] = to_signature!(testThing);
 const TEST3: &[u8] = to_signature!(testThing(1, 2, 3, 5));
+const TEST4: &[u8] = to_signature!(testThing(vec![1.0]));
 
 fn main() {
     assert_eq!(TEST, b"testThing()\0");
     assert_eq!(TEST2, b"testThing\0");
     assert_eq!(TEST3, b"testThing(_,_,_,_)\0");
-    assert_eq!(to_signature!(testThing(vec![1.0])), b"testThing(_)\0");
+    assert_eq!(TEST4, b"testThing(_)\0");
 }
