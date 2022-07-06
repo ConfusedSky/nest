d83b2dba8a3187256032014a0848ed8de7778109
Masa Maeda <masa@mortgagecoach.com>
2022-07-05 23:14:31 -0700

    adds wrapper benchmarks

--- benches/foreign_call_benchmarks.rs
+++ benches/foreign_call_benchmarks.rs
@@ -1,40 +1,27 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{
+    criterion_group, criterion_main, measurement::WallTime, BenchmarkGroup, Criterion,
+};
 use wren::{
     context::{Foreign, Native},
-    test::create_test_vm,
-    CallHandle, GetValue, Handle, WrenType,
+    test::{create_test_vm, UserData},
+    CallHandle, ForeignMethod, GetValue, Handle,
 };
 use wren_sys as ffi;
 
-fn raw_ffi<'wren, G: GetValue<'wren, Native>>(
-    context: *mut ffi::WrenVM,
-    test: *mut ffi::WrenHandle,
-    add_three: *mut ffi::WrenHandle,
-) {
-    unsafe {
-        ffi::wrenEnsureSlots(context, 4);
+unsafe fn raw_ffi(context: wren::test::Context<Foreign>) {
+    let context = context.as_ptr();
+    let a = ffi::wrenGetSlotDouble(context, 1);
+    let b = ffi::wrenGetSlotDouble(context, 2);
+    let c = ffi::wrenGetSlotDouble(context, 3);
+    ffi::wrenSetSlotDouble(context, 0, a + b + c);
+}
 
-        ffi::wrenSetSlotHandle(context, 0, test);
-        ffi::wrenSetSlotDouble(context, 1, black_box(1.0_f64));
-        ffi::wrenSetSlotDouble(context, 2, black_box(2.0_f64));
-        ffi::wrenSetSlotDouble(context, 3, black_box(3.0_f64));
+unsafe fn unchecked_raw(mut context: wren::test::Context<Foreign>) {
+    let a = f64::get_slot_raw(&mut context, 1, wren::WrenType::Num);
+    let b = f64::get_slot_raw(&mut context, 2, wren::WrenType::Num);
+    let c = f64::get_slot_raw(&mut context, 3, wren::WrenType::Num);
 
-        let result = ffi::wrenCall(context, add_three);
-
-        match result {
-            ffi::WrenInterpretResult_WREN_RESULT_SUCCESS => (),
-            _ => panic!("Interpret failed"),
-        }
-
-        // This is a hack to make sure that () doesn't get handle the return path
-        if std::mem::size_of::<G>() != 0 {
-            let mut len = std::mem::MaybeUninit::uninit();
-            let res = ffi::wrenGetSlotBytes(context, 0, len.as_mut_ptr()).cast();
-            let len = len.assume_init().try_into().unwrap();
-            let slice = std::slice::from_raw_parts(res, len);
-            let _str = String::from_utf8_lossy(slice).to_string();
-        }
-    }
+    context.set_return_value(&(a + b + c));
 }
 
 unsafe fn unchecked(mut context: wren::test::Context<Foreign>) {
@@ -59,57 +46,44 @@ fn callback<'wren>(
     assert_eq!(result, 6.0_f64);
 }
 
+fn setup<'wren>(
+    group: &mut BenchmarkGroup<WallTime>,
+    name: &str,
+    foreign: ForeignMethod<'wren, UserData<'wren>>,
+) {
+    let (mut vm, test) = create_test_vm(
+        "class Test {
+            static add_three() { add_three_(1, 2, 3) }
+            static add_three_wrapped() { add_three_wrapper(1, 2, 3) }
+
+            static add_three_wrapper(a, b, c) {
+                if ( !(a is Num) || !(b is Num) || !(c is Num)) {
+                    Fiber.abort(\"All arguments must be numbers\")
+                }
+                return add_three_(a, b, c)
+            }
+            foreign static add_three_(a, b, c)
+        }",
+        |vm| vm.set_static_foreign_method("add_three_(_,_,_)", foreign),
+    );
+    let context = vm.get_context();
+    let add_three = context.make_call_handle(wren::cstr!("add_three()"));
+    let add_three_wrapped = context.make_call_handle(wren::cstr!("add_three_wrapped()"));
+
+    let bench_2_name = name.to_string() + " Wrapped";
+
+    group.bench_function(name, |b| b.iter(|| callback(context, &test, &add_three)));
+    group.bench_function(&bench_2_name, |b| {
+        b.iter(|| callback(context, &test, &add_three_wrapped))
+    });
+}
+
 pub fn foreign_call(c: &mut Criterion) {
-    // let (mut vm, test) = create_test_vm(
-    // "class Test {
-    // static add_three() { (a+b+c).toString }
-    // foreign static add_three_(a, b, c)
-    // }",
-    // |_| {},
-    // );
-    // let context = vm.get_context();
-    // let add_three = context.make_call_handle(wren::cstr!("add_three()"));
-
-    // let context_ptr = context.as_ptr();
-    // let test_ptr = test.as_ptr();
-    // let add_three_ptr = add_three.as_ptr();
-
     let mut group = c.benchmark_group("Foreign Call");
-    // group.bench_function("Raw FFI", |b| {
-    // b.iter(|| raw_ffi::<String>(context_ptr, test_ptr, add_three_ptr));
-    // });
-
-    // group.bench_function("Unchecked Raw", |b| {
-    // b.iter(|| raw::<String>(context, &test, &add_three, WrenType::String))
-    // });
-
-    let (mut vm, test) = create_test_vm(
-        "class Test {
-            static add_three() { add_three_(1, 2, 3) }
-            foreign static add_three_(a, b, c)
-        }",
-        |vm| vm.set_static_foreign_method("add_three_(_,_,_)", unchecked),
-    );
-    let context = vm.get_context();
-    let add_three = context.make_call_handle(wren::cstr!("add_three()"));
-
-    group.bench_function("Unchecked", |b| {
-        b.iter(|| callback(context, &test, &add_three))
-    });
-
-    let (mut vm, test) = create_test_vm(
-        "class Test {
-            static add_three() { add_three_(1, 2, 3) }
-            foreign static add_three_(a, b, c)
-        }",
-        |vm| vm.set_static_foreign_method("add_three_(_,_,_)", checked),
-    );
-    let context = vm.get_context();
-    let add_three = context.make_call_handle(wren::cstr!("add_three()"));
-
-    group.bench_function("Checked", |b| {
-        b.iter(|| callback(context, &test, &add_three))
-    });
+    setup(&mut group, "Raw FFI", raw_ffi);
+    setup(&mut group, "Unchecked Raw", unchecked_raw);
+    setup(&mut group, "Unchecked", unchecked);
+    setup(&mut group, "Checked", checked);
 }
 
 criterion_group!(benches, foreign_call);
